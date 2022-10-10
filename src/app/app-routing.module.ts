7a30ca6d9b6c0db1d65e76f160505c3192aa49cc
Suraj Rawat <suraj.rawat@nagarro.com>
2022-10-10 23:42:56 +0530

    Lazy loading added to the project to improve performance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     loadChildren: () =>
       import(`./modules/pokemon-dashboard/pokemon-dashboard.module`).then(
         (module) => module.PokemonDashboardModule
-      ),
+      )
   },
 ];
 @NgModule({
